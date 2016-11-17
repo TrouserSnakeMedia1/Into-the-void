f1f83d02bedff24319b508bccd611c6af0db1f82
PeteyPablo7 <pgartzke@gmail.com>
2016-11-17 10:31:51 -0600

    Merge remote-tracking branch 'refs/remotes/origin/CameraFollow' into Development
    # Conflicts:
    #	Assets/Prefabs/Player.prefab
    #	Assets/Scenes/First Level.unity
    #	Assets/Scripts/enemyscript.cs
    #	Assets/Tiled2Unity/Imported/Chase Hallway.tiled2unity.xml
    #	Assets/Tiled2Unity/Imported/First part.tiled2unity.xml
    #	Assets/Tiled2Unity/Imported/cgf puzzle.tiled2unity.xml
    #	Assets/Tiled2Unity/Meshes/Chase Hallway.obj
    #	Assets/Tiled2Unity/Meshes/First part.obj
    #	Assets/Tiled2Unity/Meshes/First part.obj.meta
    #	Assets/Tiled2Unity/Meshes/cgf puzzle.obj
    #	Assets/Tiled2Unity/Prefabs/Chase Hallway.prefab
    #	Assets/Tiled2Unity/Prefabs/First part.prefab
    #	Assets/Tiled2Unity/Prefabs/Game Maze (1).prefab
    #	Assets/Tiled2Unity/Prefabs/cgf puzzle.prefab

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    GameObject target; // what the camera will be following
+    [SerializeField]
+    Vector3 cameraOffset; //will be able to adjust where the camera sits over the player
+	void Update () {
+        transform.position = new Vector3(target.transform.position.x + cameraOffset.x , target.transform.position.y + cameraOffset.y, target.transform.position.z +cameraOffset.z); // you are using the targets position plus an offset. the offset will create the distance between the player and the camera
+	}
+}
