6b6460bc44ffbc223149037741f572f1f957ce51
Myersm6 <myersm6@gmatc.matc.edu>
2016-12-04 11:38:12 -0600

    Terminal Changes
    Added the bools for the terminal to tell if the player is at the right
    terminal to read the correct entries

--- Assets/Scripts/ConsoleController.cs
+++ Assets/Scripts/ConsoleController.cs
@@ -165,29 +165,64 @@ public class ConsoleController
 
     void Log1(string[] args)
     {
-        appendLogLine("Technician Login: James Stevenson: ");
-        appendLogLine("I’m not too sure what is going on here. I was called in for a routine virus/bug check-up this morning, but something seems to have gone very wrong. After I was done with my checkup and ready to logout, I realized that my logout function has been disabled. I am starting to worry at this point, because I have been trapped in this system for hours now. Also, I have been noticing extreme signs of system corruption the more I look around. I will keep looking around and report what I have learned in a little bit.");
+        bool viewLogOne = GameObject.FindGameObjectWithTag("Player").GetComponent<terminalInteraction>().terminal1;
+        if (viewLogOne == true)
+        {
+            appendLogLine("Works");
+        }
+        else
+        {
+            appendLogLine("Does not work");
+        }
+
     }
     void Log2(string[] args)
     {
-        appendLogLine("Technician Login: Thomas Klein");
-        appendLogLine("I am not sure what exactly it is, but there is something completely wrong with this system. It was just fine when I logged in a few hours ago, but it has been getting progressively worse. On top of that, my basic logout function seems to not be working and I cannot figure out why. I have never seen such extreme system corruption in all of my years as a technician, and I do not believe that I can do anything to stop it at this point. I need to get out of this system very soon because corruption of this magnitude will cause the system to become extremely unstable. When this happens there will be no way to block access from any number of harmful viruses, and I would be in grave danger. I must keep searching and find the source of the corruption before it is too late….");
+        bool viewLogTwo = GameObject.FindGameObjectWithTag("Player").GetComponent<terminalInteraction>().terminal2;
+        if (viewLogTwo == true)
+        {
+            appendLogLine("Works");
+        }
+        else
+        {
+            appendLogLine("Does not work");
+        }
     }
     void Log3(string[] args)
     {
-        appendLogLine("Technician Name: John Loomis");
-        appendLogLine("Somebody needs to get me out of here now!! The system has gone completely haywire and has been infected by countless viruses and bugs. They are getting more advanced and violent by the second, and they are even starting to attack! I know this sounds completely insane, but I am telling you that these are not normal viruses and bugs. They are extremely advanced and will not hesitate to engage violently once they sense your presence. I have retreated into this room after my partner and I encountered a group of these hostile viruses, but unfortunately my partner did not make it. All I heard was his scream for a second and then I heard nothing. I can’t believe this is happening. I’m so scared. My partner said he had discovered a hidden room just down the hall. Maybe that would be the best place to hide and figure out my next move.");
+        bool viewLogThree = GameObject.FindGameObjectWithTag("Player").GetComponent<terminalInteraction>().terminal3;
+        if (viewLogThree == true)
+        {
+            appendLogLine("Works");
+        }
+        else
+        {
+            appendLogLine("Does not work");
+        }
     }
     void Log4(string[] args)
     {
-        appendLogLine("Technician Name: John Loomis");
-        appendLogLine("I didn’t sign up for this!! This was never supposed to be part of the job!! I just want to get out of this damn nightmare and go home!! Those damn things out there have trapped me in this room, and it is only a matter of time before they get to me. Why is this happening? I know that me and my partner were not the first ones sent here, because I passed by many other bodies while running away from those things! How could there be other people in this system? Me and my partner were the only ones assigned to this job, but yet I know what I saw. Those bastards will break through that door any minute now. If anyone happens to find this, please tell my wife and kids that I love them and I’m very sorry for leaving them so soon...");
+        bool viewLogFour = GameObject.FindGameObjectWithTag("Player").GetComponent<terminalInteraction>().terminal4;
+        if (viewLogFour == true)
+        {
+            appendLogLine("Works");
+        }
+        else
+        {
+            appendLogLine("Does not work");
+        }
     }
     void Log5(string[] args)
     {
-        appendLogLine("Technician Login: Wayan Seymour");
-        appendLogLine("*****ATTENTION POTENTIAL VICTIMS OF THIS HELLSCAPE*****");
-        appendLogLine("It’s obvious that, for whatever asinine reason, VSI isn’t going to stop sending people here. It’s also possible that there are living people in here that I just haven’t found yet. In either case, if you’re reading this, I’d like to inform you that any messages, notes, or reports you types into the text program on these terminals is NOT visible or retrievable to anyone on the outside. No one is going to datamine this system and find any readable form of what you’ve written, even if they knew to look for it, which they almost certainly don’t. There are better ways to use these terminals than wailing your despair into the void. There’s got to be a backdoor or something. A way to get to the locked user options. And we should be able to find a way to it through these terminals. I’m close to finding a way to get the command window working again on the terminal in the keycard room. If I can get it running and manage to unlock the user controls, then I guess this message is pointless. But if you are reading this, and you want to get out of here alive, I suggest you stop pleading through a text document no one will read, head over to that room, and contribute to your one chance of getting out of here alive.");
+        bool viewLogFive = GameObject.FindGameObjectWithTag("Player").GetComponent<terminalInteraction>().terminal5;
+        if (viewLogFive == true)
+        {
+            appendLogLine("Works");
+        }
+        else
+        {
+            appendLogLine("Does not work");
+        }
     }
     void help(string[] args)
     {
--- Assets/Scripts/terminalInteraction.cs
+++ Assets/Scripts/terminalInteraction.cs
@@ -4,22 +4,96 @@ using System.Collections;
 public class terminalInteraction : MonoBehaviour {
 
     public bool inProximity = false;
+    public bool terminal1 = false;
+    public bool terminal2 = false;
+    public bool terminal3 = false;
+    public bool terminal4 = false;
+    public bool terminal5 = false;
+    public bool terminal6 = false;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Terminal") {
+        /*if (other.tag == "Terminal") {
             print("Player collided with terminal.");
             this.inProximity = true;
             GetComponent<AudioSource>().Play(); // plays the audio source attached to the gameobject with this script in this case the terminal object it also plays based on proximity due to where this code is placed
+        }*/
+
+        if(other.tag == "Terminal#1")
+        {
+            this.inProximity = true;
+            terminal1 = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.tag == "Terminal#2")
+        {
+            this.inProximity = true;
+            terminal2 = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.tag == "Terminal#3")
+        {
+            this.inProximity = true;
+            terminal3 = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.tag == "Terminal#4")
+        {
+            this.inProximity = true;
+            terminal4 = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.tag == "Terminal#5")
+        {
+            this.inProximity = true;
+            terminal5 = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.tag == "Terminal#6")
+        {
+            this.inProximity = true;
+            terminal6 = true;
+            GetComponent<AudioSource>().Play();
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Terminal") {
+        /*if (other.tag == "Terminal") {
             print("Player left the terminal.");
             this.inProximity = false;
             //clicks = 0;
+        }*/
+
+        if (other.tag == "Terminal#1")
+        {
+            this.inProximity = false;
+            terminal1 = false;
+        }
+        if (other.tag == "Terminal#2")
+        {
+            this.inProximity = false;
+            terminal2 = false;
+        }
+        if (other.tag == "Terminal#3")
+        {
+            this.inProximity = false;
+            terminal3 = false;
+        }
+        if (other.tag == "Terminal#4")
+        {
+            this.inProximity = false;
+            terminal4 = false;
+        }
+        if (other.tag == "Terminal#5")
+        {
+            this.inProximity = false;
+            terminal5 = false;
+        }
+        if (other.tag == "Terminal#6")
+        {
+            this.inProximity = false;
+            terminal6 = false;
         }
     }
 }
