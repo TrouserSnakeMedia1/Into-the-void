ca03d2dd56ec5455ba958bf24a6e967b5a5419a2
PeteyPablo7 <pgartzke@gmail.com>
2016-12-10 23:51:19 -0600

    updated chase cut scene

---
+++ Assets/Scripts/SlimeAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeAnimation : MonoBehaviour {
+
+    public Animator anim;
+    GameObject playerModel;
+    bool pushIt;
+ 
+    // Use this for initialization
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>(); // finds and sets the animator named anim to equal animator that is attached to this gameobject this script is attached to
+
+
+    }
+    void Start () {
+       playerModel = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pushIt = playerModel.GetComponent<Chase>().slimeSpawn;
+        if(pushIt==true)
+        {
+            anim.SetInteger("SlimeSwitch", 3);
+        }
+        else
+        {
+            anim.SetInteger("SlimeSwitch", 2);
+        }
+    }
+}
---
+++ Assets/Scripts/SlimeFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeFollow : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject target; // what the camera will be following
+    [SerializeField]
+    Vector3 cameraOffset; //will be able to adjust where the camera sits over the player
+    GameObject mainCamera;
+    bool pushIt;
+    GameObject thePlayer;
+    // Use this for initialization
+    void Start () {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        pushIt = thePlayer.GetComponent<Chase>().slimeSpawn; // sets the bool pushIt to equal the bool push thats in this script
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (pushIt == true)
+        {
+            mainCamera.transform.position = new Vector3(target.transform.position.x + cameraOffset.x, target.transform.position.y + cameraOffset.y, target.transform.position.z + cameraOffset.z); // you are using the targets position plus an offset. the offset will create the distance between the player and the camera
+        }
+    }
+    public void followSlime()
+    {
+       
+        }
+}
