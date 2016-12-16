7d3c8be357aa349404123fbdc8c7e7a40cb6c797
PeteyPablo7 <pgartzke@gmail.com>
2016-12-16 09:51:05 -0600

    lets hope

---
+++ Assets/Final CGF Assets/EndBank.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndBank : CGFManager {
+    public bool foxBool, chickenBool, grainBool, playerBool;
+    //GameObject fox, chicken, grain, player;
+    CGFManager cgfManager;
+    void Start()
+    {
+        foxBool = false;
+        chickenBool = false;
+        grainBool = false;
+        playerBool = false;
+        //fox = GameObject.FindGameObjectWithTag("Fox");
+        //chicken = GameObject.FindGameObjectWithTag("Chicken");
+        //grain = GameObject.FindGameObjectWithTag("Grain");
+        //player = GameObject.FindGameObjectWithTag("Player");
+        cgfManager = GetComponent<CGFManager>();
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Fox")
+    //    {
+    //        foxBool = true;
+    //        cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Chicken")
+    //    {
+    //        chickenBool = true;
+    //        cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Grain")
+    //    {
+    //        grainBool = true;
+    //        cgfManager.EndCGFState(foxBool, chicken, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Player")
+    //    {
+    //        playerBool = true;
+    //        cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Fox")
+        {
+            foxBool = true;
+            //cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Chicken")
+        {
+            chickenBool = true;
+            //cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Grain")
+        {
+            grainBool = true;
+            //cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Player")
+        {
+            playerBool = true;
+            //cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Fox")
+        {
+            foxBool = false;
+            cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Chicken")
+        {
+            chickenBool = false;
+            cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Grain")
+        {
+            grainBool = false;
+            cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Player")
+        {
+            playerBool = false;
+            cgfManager.EndCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+    }
+}
---
+++ Assets/Final CGF Assets/StartBank.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBank : CGFManager
+{
+    public bool foxBool, chickenBool, grainBool, playerBool;
+    //public GameObject fox, chicken, grain, player;
+    CGFManager cgfManager;
+    void Start()
+    {
+        foxBool = true;
+        chickenBool = true;
+        grainBool = true;
+        playerBool = false;
+        //fox = GameObject.FindGameObjectWithTag("Fox");
+        //chicken = GameObject.FindGameObjectWithTag("Chicken");
+        //grain = GameObject.FindGameObjectWithTag("Grain");
+        //player = GameObject.FindGameObjectWithTag("Player");
+        cgfManager = GetComponent<CGFManager>();
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Fox")
+    //    {
+    //        foxBool = true;
+    //        cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Chicken")
+    //    {
+    //        chickenBool = true;
+    //        cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Grain")
+    //    {
+    //        grainBool = true;
+    //        cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //    if (other.tag == "Player")
+    //    {
+    //        playerBool = true;
+    //        cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+    //    }
+    //}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Fox")
+        {
+            foxBool = true;
+           // cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Chicken")
+        {
+            chickenBool = true;
+            //cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Grain")
+        {
+            grainBool = true;
+            //cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Player")
+        {
+            playerBool = true;
+            //cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Fox")
+        {
+            foxBool = false;
+            cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Chicken")
+        {
+            chickenBool = false;
+            cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Grain")
+        {
+            grainBool = false;
+            cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+        if (other.tag == "Player")
+        {
+            playerBool = false;
+            cgfManager.StartCGFState(foxBool, chickenBool, grainBool, playerBool);
+        }
+    }
+}
